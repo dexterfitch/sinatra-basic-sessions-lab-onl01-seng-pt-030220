535f2fd0265ff7751e9d583e7452f9875f0edbba
Dexter Fitch <fitch.dexter@gmail.com>
2020-06-01 17:53:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,4 +7,8 @@ class App < Sinatra::Base
     set :session_secret, "happypride884815b4bcfeca12c8fee512c02111825b82ebae06dc9bc4a3a8b7219d401a807e0184172d2fe135d251b5503c08220f907abf290d9604deaa011e3d687e3c5c"
   end
 
+  get '/' do
+    erb :index
+  end
+
 end
