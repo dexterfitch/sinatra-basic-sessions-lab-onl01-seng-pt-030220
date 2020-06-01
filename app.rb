c621d47c5195c6cf03a223e902bb458af8af72d5
Dexter Fitch <fitch.dexter@gmail.com>
2020-06-01 17:52:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, "happypride!884815b4bcfeca12c8fee512c02111825b82ebae06dc9bc4a3a8b7219d401a807e0184172d2fe135d251b5503c08220f907abf290d9604deaa011e3d687e3c5c"
+    set :session_secret, "happypride884815b4bcfeca12c8fee512c02111825b82ebae06dc9bc4a3a8b7219d401a807e0184172d2fe135d251b5503c08220f907abf290d9604deaa011e3d687e3c5c"
   end
 
 end
